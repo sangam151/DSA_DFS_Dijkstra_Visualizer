bc09a63fd93eb33044dbd7dd2e0dd12cc6436509
maheshpanta333 <pantamahesh99@gmail.com>
2026-02-23 20:23:47 +0545

    added basic structure for ui

--- headers/mainWindow.h
+++ headers/mainWindow.h
@@ -1,1 +1,38 @@
+// mainWindow.h
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
 
+#include <QMainWindow>
+#include <QTimer>
+#include <QElapsedTimer>
+#include <QTableWidget>
+#include <QTableWidgetItem>
+#include <QPushButton> // Includes QPushButton for the new button
+#include "GridView.h"
+#include "PathAlgorithm.h"
+#include "qlabel.h"
+#include <QFile>
+#include <QTextStream>
+#include "PlayerMazeWindow.h" // Includes the new player maze window
+
+QT_BEGIN_NAMESPACE
+
+namespace Ui
+{
+class MainWindow;
+}
+
+QT_END_NAMESPACE
+
+
+//structure to hold comparison data for each algorithm run
+struct AlgorithmComparisonData {
+    QString algorithmName;
+    qint64  timeElapsedMs;
+    int     nodesVisited;
+    int     pathLength;
+    QString gridSize;
+    qreal   wallDensity;
+    int     numDeadEnds;
+    qreal   branchingFactor;
+};
