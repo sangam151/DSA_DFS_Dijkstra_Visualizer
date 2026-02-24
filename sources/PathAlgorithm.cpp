65678cebc834b4ae12efac7825e1956fd1e287be
maheshpanta333 <pantamahesh99@gmail.com>
2026-02-24 09:21:40 +0545

    added more logic to connect algorithm with backend for shortest path algorithm basically adding stuff to run dijikstra but not added dijikstras yet

--- sources/PathAlgorithm.cpp
+++ sources/PathAlgorithm.cpp
@@ -70,3 +70,125 @@ void PathAlgorithm::setGridNodes(const grid& newGridNodes, int width, int height
         node.nextUp = false;
     }
 }
+
+std::vector<Node> PathAlgorithm::retrieveNeighborsGrid(const grid& gridNodes, const Node& currentNode, int widthGrid, int heightGrid)
+{
+
+    std::vector<Node> neighbors;
+
+    // right: adding +1 to x:
+    if (currentNode.xCoord + 1 <= widthGrid)
+    {
+        int rightIndex = coordToIndex(currentNode.xCoord + 1, currentNode.yCoord, widthGrid);
+        neighbors.push_back(gridNodes.Nodes[rightIndex]);
+
+    }
+
+    // down: adding -1 to y:
+    if (currentNode.yCoord - 1 >= 1)
+    {
+        int downIndex = coordToIndex(currentNode.xCoord, currentNode.yCoord -1, widthGrid);
+        neighbors.push_back(gridNodes.Nodes[downIndex]);
+    }
+
+    // left: adding -1 to x:
+    if (currentNode.xCoord - 1 >= 1)
+    {
+        int leftIndex = coordToIndex(currentNode.xCoord - 1, currentNode.yCoord, widthGrid);
+        neighbors.push_back(gridNodes.Nodes[leftIndex]);
+    }
+
+    // up: adding +1 to y:
+    if (currentNode.yCoord + 1 <= heightGrid)
+    {
+        int upIndex = coordToIndex(currentNode.xCoord, currentNode.yCoord + 1, widthGrid);
+        neighbors.push_back(gridNodes.Nodes[upIndex]);
+    }
+
+    return neighbors;
+}
+
+QString PathAlgorithm::algorithmToString(ALGOS algo) {
+    switch (algo) {
+    case BFS: return "BFS";
+    case DFS: return "DFS";
+    case DIJKSTRA: return "DIJKSTRA";
+    case ASTAR: return "ASTAR";
+    case BACKTRACK: return "BACKTRACK";
+    case NOALGO: return "NOALGO";
+    default: return "UNKNOWN_ALGO";
+    }
+}
+void PathAlgorithm::checkGridNode(grid gridNodes, int heightGrid, int widthGrid)
+{
+    // Display grid
+    std::cerr << "State of grid node \n";
+    int countVisited = 0; int countObstacle = 0; int countFree = 0;
+
+    for (Node node: gridNodes.Nodes)
+    {
+        std::cerr << "(" << node.xCoord << ", " <<  node.yCoord << "): ";
+
+        if (node.visited){std::cerr << ": V"; countVisited++;}
+
+        if (node.obstacle){std::cerr << ": O"; countObstacle++;}
+        else{std::cerr << ": F"; countFree++;}
+
+        if (node.xCoord == widthGrid){std::cerr << " \n";}
+        else{std::cerr << " | ";}
+
+    }
+    std::cerr << "Totals: " << "Visited: " << countVisited
+              << " - Obstacles: " << countObstacle
+              << " - Free:" << countFree << "\n";
+
+    // Check size of vector
+    if (static_cast<int>(gridNodes.Nodes.size()) != static_cast<int>(heightGrid * widthGrid))
+    {std::cerr << "Number of nodes in gridNodes: " << gridNodes.Nodes.size() << " vs " << heightGrid * widthGrid << " [ISSUE] \n";}
+    else{std::cerr << "Number of nodes in gridNodes: " << gridNodes.Nodes.size() << "\n";}
+
+}
+
+void PathAlgorithm::runAlgorithm(ALGOS algorithm)
+{
+    simulationOnGoing=true;
+    running=true;
+    qDebug() << "PathAlgorithm: runAlgorithm called. Thread ID:" << QThread::currentThreadId();
+
+    switch (algorithm) {
+
+    case DIJKSTRA:
+        futureOutput = QtConcurrent::run(&pool, &PathAlgorithm::performDijkstraAlgorithm, this);
+        break;
+
+
+    case NOALGO:
+        std::cerr <<"NO ALGO \n";
+    default:
+        break;
+
+    }
+
+}
+
+void PathAlgorithm::resumeAlgorithm()
+{
+    running = true;
+    futureOutput.resume();
+    qDebug() << "PathAlgorithm: futureOutput.resume() called.";
+}
+
+void PathAlgorithm::pauseAlgorithm()
+{
+    running = false;
+    futureOutput.suspend();
+    qDebug() << "PathAlgorithm: futureOutput.suspend() called.";
+}
+
+void PathAlgorithm::stopAlgorithm()
+{
+    running = false;
+    futureOutput.cancel();
+    qDebug() << "PathAlgorithm: futureOutput.cancel() called.";
+}
+
